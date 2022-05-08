844981cc787f6a50632a0ca86b3e078d7bdb1cd6
Harvinder Singh <69596941+Hackersvilla@users.noreply.github.com>
2022-05-08 09:03:21 +0530

    done

--- urlshortie/src/Components/LoginPage.js
+++ urlshortie/src/Components/LoginPage.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
+  Button,
   InputGroup,
   InputLeftElement,
   Stack,
@@ -8,17 +9,25 @@ import {
 } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { SiGmail } from "react-icons/si";
-import {RiLockPasswordFill} from 'react-icons/ri'
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
 
 export default function LoginPage() {
   return (
     <>
       <Box w={500} h={500} bg="white" boxShadow="2xl" borderRadius={10}>
-        <Stack direction="column" p={30} spacing="30">
+        <Stack direction="column" p={30} spacing={3}>
           <Text fontSize="30" fontFamily="Koulen, sans-serif">
             Sign Up
           </Text>
           <Stack direction="column" p={30}>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<FaUser />}
+              ></InputLeftElement>
+              <Input variant="filled" placeholder="Name" />
+            </InputGroup>
 
             <InputGroup>
               <InputLeftElement
@@ -28,7 +37,6 @@ export default function LoginPage() {
               <Input variant="filled" placeholder="Email" />
             </InputGroup>
 
-
             <InputGroup>
               <InputLeftElement
                 pointerEvents="none"
@@ -37,9 +45,23 @@ export default function LoginPage() {
               <Input variant="filled" placeholder="Password" />
             </InputGroup>
 
-
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<RiLockPasswordFill />}
+              ></InputLeftElement>
+              <Input variant="filled" placeholder="Confirm Password" />
+            </InputGroup>
           </Stack>
-
+          <Stack direction="column">
+            <Button bg="#85586F" color="white">
+              Sign Up
+            </Button>
+            <Text textAlign="center">----------Or------------</Text>
+            <Button bg="#85586F" color="white">
+              Sign In
+            </Button>
+          </Stack>
         </Stack>
       </Box>
     </>
